45e206973422a84b56e19f960b21019aa5124bee
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-07-07 16:53:12 +0300

    task1322

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1318/Solution.java
@@ -0,0 +1,44 @@
+package com.javarush.task.task13.task1318;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+/* 
+Чтение файла
+1. Считать с консоли имя файла.
+2. Вывести в консоль(на экран) содержимое файла.
+3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
+
+
+Требования:
+1. Программа должна считывать c консоли имя файла.
+2. Программа должна выводить на экран содержимое файла.
+3. Поток чтения из файла (FileInputStream) должен быть закрыт.
+4. BufferedReader также должен быть закрыт.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        // напишите тут ваш код
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        FileInputStream file = null;
+        try
+        {
+            file = new FileInputStream(br.readLine());
+            while(file.available() != 0)
+            {
+                System.out.print((char)file.read());
+            }
+        }
+        catch (IOException e)
+        {
+
+        }
+
+        file.close();
+        br.close();
+    }
+}
