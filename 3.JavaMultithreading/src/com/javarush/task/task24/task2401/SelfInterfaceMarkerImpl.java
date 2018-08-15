1dad9e9db82b1e7eb80da8f36fb38007434a04f8
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-08-15 13:33:27 +0300

    task2406

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2401/SelfInterfaceMarkerImpl.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task24.task2401;
+
+public class SelfInterfaceMarkerImpl implements SelfInterfaceMarker
+{
+    public void firstMethod()
+    {
+        System.out.println("First Method");
+    }
+
+    public void secondMethod()
+    {
+        System.out.println("Second Method");
+    }
+}
