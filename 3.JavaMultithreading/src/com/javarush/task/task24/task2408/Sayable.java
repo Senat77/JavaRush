481a96b775e71fdf4491c73cfefc13cdbcfa24e2
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-08-16 16:32:23 +0300

    task2412

---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2408/Sayable.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task24.task2408;
+
+public interface Sayable
+{
+    String say();
+}
---
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2410/Iterator.java
@@ -0,0 +1,6 @@
+package com.javarush.task.task24.task2410;
+
+public interface Iterator
+{
+    Iterator next();
+}
